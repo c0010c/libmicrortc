7b69f77b72d0ac22a8a0bb63d1049d6e68eb6feb
QS <c228846384@gmail.com>
2026-03-14 22:39:10 +0800

    x

--- src/platform/linux/platform_linux.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTC_PLATFORM_LINUX_H
-#define RTC_PLATFORM_LINUX_H
-
-#include "rtc/rtc_platform.h"
-
-int rtc_linux_default_ops(rtc_platform_ops_t *out_ops);
-
-#endif
--- src/platform/rtos/platform_rtos_stub.h
+++
@@ -1,8 +0,0 @@
-#ifndef RTC_PLATFORM_RTOS_STUB_H
-#define RTC_PLATFORM_RTOS_STUB_H
-
-#include "rtc/rtc_platform.h"
-
-int rtc_rtos_stub_ops(rtc_platform_ops_t *out_ops);
-
-#endif
