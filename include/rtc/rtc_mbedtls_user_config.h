75f57fdce3ea9ab847b1fed2f79448de53ac85c7
QS <c228846384@gmail.com>
2026-03-15 00:41:23 +0800

    真实 DTLS + SRTP 集成

---
+++ include/rtc/rtc_mbedtls_user_config.h
@@ -0,0 +1,7 @@
+#ifndef RTC_MBEDTLS_USER_CONFIG_H_
+#define RTC_MBEDTLS_USER_CONFIG_H_
+
+/* Enable DTLS-SRTP extension for key extraction and profile negotiation. */
+#define MBEDTLS_SSL_DTLS_SRTP
+
+#endif  // RTC_MBEDTLS_USER_CONFIG_H_
